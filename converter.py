6bc292f3db6d3efe194e995b590561da123d1488
Rostislav Rabiec <rosta.rabiec@icloud.com>
2023-06-01 16:49:34 +0200

    Updated to version 1.0.1. Created button to delete user input and result.

--- converter.py
+++ converter.py
@@ -7,8 +7,8 @@ main_color = "#07018d"
 
 # Window
 window = Tk()
-window.title("Převod měn verze 1.0.0")
-window.geometry("550x120+1000+200")
+window.title("Převod měn verze 1.0.1")
+window.geometry("550x150+1000+200")
 window.resizable(True, True)
 window.config(bg=main_color)
 
@@ -32,6 +32,10 @@ def count_currency():
     except:
         notification_label.config(text="Zadej částku.")
 
+# Function to delete user input and result
+def delete_input_and_result():
+    user_input.delete(0, END)
+    result_label.config(text="0")
 
 # User input
 user_input = Entry(width=20, font=("Arial", 20), justify=CENTER)
@@ -50,9 +54,13 @@ drop_down_to.set("CZK")
 drop_down_to_option = OptionMenu(window, drop_down_to, "CZK", "EUR", "USD", "PLN")
 drop_down_to_option.grid(row=1, column=1, padx=10, pady=(10, 0))
 
-# Button
+# Button to convert
 count_button = Button(text="Přepočítej", font=("Arial", 20), command=count_currency)
 count_button.grid(row=0, column=2, padx=10, pady=(10, 0))
+
+# Button to delete user input and result
+delete_button = Button(text="Smazat", font=("Arial", 20), command=delete_input_and_result)
+delete_button.grid(row=1, column=2, padx=10, pady=10)
 
 # Laber for result
 result_label = Label(text="0", bg=main_color, fg="white", font=("Arial", 20))
